61af25c21952671a5e5fd9e8c5f7f0b707b93115
David Neufeld <geoneubie@gmail.com>
2026-09-13 10:49:32 -0600

    Add errors for debugging

--- Ex6/ex6script.js
+++ Ex6/ex6script.js
@@ -1,19 +1,21 @@
-function parseSourceParam() {
+function parseResortParam() {
 
-        const viewElement = document.querySelector("arcgis-map");
+    const viewElement = document.querySelector("arcgis-map");
 
-        const locString = window.location;
-        console.log("locString=" + locString);
-        const urlParams = new URLSearchParams(locString.search);
-        let resort = urlParams.get("resort");
-        console.log(resort);
-        if (resort=="winterpark") {
-            viewElement.goTo(
-                // go to point with a custom animation duration
-                { center: [-105.76, 39.89] },
-                { duration: 5000 },
-            );
-        }
-        viewElement.zoom = 15;
+    const locString = window.location;
+    console.log("locString=" + locString);
+    const urlParams = new URLSearchParams(locString.search);
+    let resort = urlParams.get("resort");
+    console.log(resort);
+    if (resort=="winterpark") {
+        viewElement.goTo(
+            // go to point with a custom animation duration
+            { center: [-105.76 39.89] },
+            { duration: 5000 },
+        );
+    
+    viewElement.zoom = 15;
   
 }
+
+parseResortParam();
