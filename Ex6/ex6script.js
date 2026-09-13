899118bebaba474d42c508c519e81d4caa950d68
David Neufeld <geoneubie@gmail.com>
2026-09-13 09:58:56 -0600

    Refactor map handling and update resort logic

--- Ex6/ex6script.js
+++ Ex6/ex6script.js
@@ -1,32 +1,19 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiZG5ldWZlbGQtZnJjYyIsImEiOiJjbTBmeDY4cDcxNHdhMmtvaXRmdjJqeDltIn0.TQpjFnvI8qiW1J5OEmGJIA';
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/dneufeld-frcc/cmezze56b011t01ssezwt9ial',
-  center: [-105, 39.7], 
-  zoom: 8
-});
-
 function parseSourceParam() {
 
-  const locString = window.location;
-  console.log("locString=" + locString);
-  
-  const urlParams = new URLSearchParams(locString.search);
-  var source = urlParams.get("source");
-  console.log(source);
+        const viewElement = document.querySelector("arcgis-map");
 
-  // Add conditionals
-  if (source == "winterpark") { 
-    map.setCenter([-105.8171, 39.9135]);
-    map.setZoom(9);
-    document.getElementById("skiText").interHTML = "Ski Area: Winter Park"
-  }
-
-}
-if (source == "vail") { 
-  map.setCenter([-106.319,39.6446]);
-  map.setZoom(9);
-  document.getElementById("skiText").innerHTML = "Ski Area: Vail"
-}
+        const locString = window.location;
+        console.log("locString=" + locString);
+        const urlParams = new URLSearchParams(locString.search);
+        let resort = urlParams.get("resort");
+        console.log(resort);
+        if (resort=="winterpark") {
+            viewElement.goTo(
+                // go to point with a custom animation duration
+                { center: [-105.76, 39.89] },
+                { duration: 5000 },
+            );
+        }
+        viewElement.zoom = 15;
   
 }
